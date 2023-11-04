4d3d456e64b729c0439e238e907859006b7cda28
gus <gustitelez@hotmail.com>
2023-11-04 16:26:42 +0000

    settings update production working

--- banco_cf/settings.py
+++ banco_cf/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,6 +130,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS= [ BASE_DIR / 'staticfiles']
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
